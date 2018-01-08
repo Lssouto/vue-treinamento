b4097ea077e0f24665772a468bcd41aa61c69a34
Lucas Souto <lssouto96@gmail.com>
2018-01-08 19:46:49 +0000

    new navbar style

--- public/src/components/global/Navbar.vue
+++ public/src/components/global/Navbar.vue
@@ -3,7 +3,14 @@
     <ul>
       <li><router-link to="/" >Home</router-link></li>
       <li><router-link to="/Register" >Register</router-link></li>
-      <li><router-link to="/Games">Games</router-link></li>
+      <li>
+        <input type="checkbox" name="games" id="games" />
+        <label for="games">Games</label>
+        <ul>
+          <li><router-link to="/Games">Listar</router-link></li>
+          <li><router-link to="/Games/adicionar">Adicionar</router-link></li>
+        </ul>
+      </li>
     </ul>
     
   </div>
