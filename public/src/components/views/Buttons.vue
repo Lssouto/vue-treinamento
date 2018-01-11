205a39cf26ade2295bf3d7a936f325122511bbc8
Lucas Souto <lssouto96@gmail.com>
2018-01-11 13:32:47 +0000

    buttons full dynamic color

--- public/src/components/views/Buttons.vue
+++ public/src/components/views/Buttons.vue
@@ -1,66 +1,100 @@
 <template>
-  <div id="buttons" class="container-fluid">
-      <div class="row">
-          <div class="col-md-6">
-                <h2>Click effect</h2>
+    <div id="buttons" class="container-fluid">
+        <div class="row">
+            <div class="col-md-6">
+                <button class="btn btn-effect" :style="colorsObject"> Simple</button>
                 <button class="btn btn-effect _pass-by _left" :style="colorsObject"> Pass By</button>
-                <button class="btn btn-effect _pulse" :style="colorsObject"> Pulse</button>
-                <h2>Hover effect</h2>
                 <button class="btn btn-effect _stop-inside _right" :style="colorsObject">Stop inside</button>
-                <h2>Alterar cor</h2>
-                <button class="btn" @click="changeColor()" :style="colorsObject">Alterar</button>
-          </div>
-          <div class="col-md-6">
+                <button class="btn btn-effect _pulse" :style="colorsObject"> Pulse</button>
+                <button class="btn btn-effect _pulse" :style="colorsObject"><span class="scaled">Font Up</span></button>
+            </div>
+            <div class="col-md-6">
                 <div class="photoshop-container">
                     <h3>Background</h3>
-                    <photoshop-picker v-model="backgroundColors"> </photoshop-picker>
+                    <chrome-picker v-model="backgroundColors"/>
                 </div>
                 <div class="photoshop-container">
                     <h3>Text</h3>
-                    <photoshop-picker v-model="textColors"> </photoshop-picker>
+                    <chrome-picker v-model="textColors"/>
                 </div>
-          </div>
-      </div>
-  </div>
+                <div class="photoshop-container">
+                    <h3>Effect</h3>
+                    <chrome-picker v-model="effectColors"/>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import { Photoshop } from 'vue-color'
+import { Chrome } from 'vue-color'
 export default {
     name : 'Buttons',
     data(){
         return{
             backgroundColors: '',
             textColors: '',
+            effectColors : '',
             colorsObject :{
                 'background-color': '#9e54bd',
-                'color': '#fff'
+                'color': '#fff',
+                'effect-color' : '#fff'
             }
         }
     },
-    components : {
-        'photoshop-picker': Photoshop
+    components: {
+        'chrome-picker': Chrome
+    },
+    mounted : function(){
+        this.$el.style.setProperty('--colorEffect', 'rgba(255,255,255,0.2)')
     },
     methods:{
         changeColor : function(){
             this.colorsObject['background-color'] = this.backgroundColors.hex
             this.colorsObject['color'] = this.textColors.hex
         }
+    },
+    watch: {
+        backgroundColors : function(){
+            this.colorsObject['background-color'] = this.backgroundColors.hex
+        },
+        textColors : function(){
+            this.colorsObject['color'] = this.textColors.hex
+        },
+        effectColors : function(){
+            this.$el.style.setProperty('--colorEffect', 
+            'rgba('
+            +   this.effectColors.rgba.r +','
+            +   this.effectColors.rgba.g +',' 
+            +   this.effectColors.rgba.b +','
+            +   this.effectColors.rgba.a +')')
+        },
+        
     }
 }
 </script>
 
 
 <style lang="scss" scoped>
+:root{
+    --colorEffect : rgba(0,0,0,0);
+}
+
 .photoshop-container{
-    display: block;
+    display: inline-block;
     position: relative;
     margin-bottom: 15px;
+    width: 49%;
+}
+#buttons .btn.btn-effect{
+    margin-top: 25px;
     h3{
-        position:absolute;
-        left: 10px;
-        top: -20px;
-        
+        margin: 0;
+    }
+    &._stop-inside,&._pulse,&._pass-by{
+        &:after{
+            background-color: var(--colorEffect); 
+        }
     }
 }
 
