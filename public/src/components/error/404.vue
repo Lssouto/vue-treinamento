20b17cbace33e38d96967331bac48bb7a6d9162e
Lucas Souto <lssouto96@gmail.com>
2018-01-24 18:29:22 +0000

    redirect add to 404  page

--- public/src/components/error/404.vue
+++ public/src/components/error/404.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
       <h1>Página não encontrada</h1>
+      <h3>Redirecionando para HomePage . . .</h3>
   </div>
 </template>
 
 <script>
 export default {
-    name : 'c404'
+    name : 'c404',
+    mounted (){
+      setTimeout(()=>{
+        this.$router.push({name: 'Index'})
+      },2000)
+    }
 }
 </script>
 
